d0be12e0e61b471678932503deda14b4f8a52f1c
sahibzadahamza <143394661+sahibzadahamza@users.noreply.github.com>
2024-04-19 16:18:27 +0500

    Reapply "Merge branch 'main' into dev_shuja"
    This reverts commit 8cf575d1d9cb9c62d56cb9b96e8b586d769f79cb.

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,6 +7,8 @@ import Login from '../pages/Login';
 import SignUp from '../pages/Signup';
 import EmployeeGrid from '../pages/EmployeesGrid';
 import JobListing from '../pages/JobListing';
+import Employee from '../pages/Employee';
+import CreateJob from '../pages/CreateJobs';
 import AddEmployee from '../pages/AddEmployee';
 import UpdateEmployee from '../pages/UpdateEmployee';
 import CreateJob from '../pages/CreateJob';
@@ -20,7 +22,6 @@ const Routers = () => {
         <Route path="/signup" element={<SignUp />} /> 
         <Route path="/employee" element={<EmployeeGrid />} /> 
         <Route path="/joblisting" element={<JobListing />} /> 
-        <Route path="/createJob" element={<CreateJob />} /> 
         <Route path="/employeesDetail" element={<EmployeeGrid />} /> 
         <Route path="/addEmployee" element={<AddEmployee />} />
         <Route path="/updateEmployee/:id" element={<UpdateEmployee />} /> 
