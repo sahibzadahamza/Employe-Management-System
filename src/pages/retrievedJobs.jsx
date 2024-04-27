7ad83c4177dd97957574624c4dbb339578e4003d
adnan khan <adnanmaneri2015@gmail.com>
2024-04-27 21:24:21 +0500

    new-code

--- src/pages/retrievedJobs.jsx
+++ src/pages/retrievedJobs.jsx
@@ -10,11 +10,8 @@ const RetrievedJobs = () => {
     const fetchJobApplications = async () => {
       try {
         const response = await axios.get(`${apiUrl}/api/applied-users`);
-        if(response.status===200)
-        {
-            setJob(response.data);
-            console.log('hello',job);
-            console.log(response.data)
+        if (response.status === 200) {
+          setJob(response.data);
         }
       } catch (error) {
         console.error('Error fetching job applications:', error);
@@ -24,21 +21,28 @@ const RetrievedJobs = () => {
     fetchJobApplications();
   }, []);
 
+  useEffect(() => {
+    console.log('Updated job:', job);
+  }, [job]); // Log the updated state when 'job' changes
+
+  console.log('Initial job:', job); // Log the initial state
+
+
   return (
     <div>
       <AdminHeader />
       <div className="mt-8">
         <h2 className="text-3xl font-bold mb-4">Job Applications</h2>
-        {/* {jobApplications?.map(application => (
+        {Array.isArray(job) && job.map(application => (
           <div key={application._id} className="bg-white shadow-md p-6 mb-4 rounded-md">
             <p><span className="font-semibold">Name:</span> {application.name}</p>
             <p><span className="font-semibold">Email:</span> {application.email}</p>
             <p><span className="font-semibold">Job Title:</span> {application.jobTitle}</p>
             <p><span className="font-semibold">Applied At:</span> {new Date(application.appliedAt).toLocaleString()}</p>
-         
+            {/* Add additional fields as needed */}
             <p className="mt-4"><span className="font-semibold">Resume:</span> <a href={`/${application.resume}`} target="_blank" rel="noopener noreferrer">View Resume</a></p>
           </div>
-        ))} */}
+        ))}
       </div>
     </div>
   );
