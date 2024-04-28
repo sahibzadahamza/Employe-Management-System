2a9e81dec9b1d795f9f688a272bc6e85fa1097c3
coder-adnan <adnanmaneri2015@gmail.com>
2024-04-28 20:31:45 +0500

    Added show appied jobs to the user

--- src/pages/ShowAppliedJobs.jsx
+++ src/pages/ShowAppliedJobs.jsx
@@ -1,48 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import UserHeader from './userHeader';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import UserHeader from "./userHeader";
 
 const ShowAppliedJobs = () => {
-    const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+  const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
   const [appliedJobs, setAppliedJobs] = useState([]);
-
-  useEffect(() => {
-    const fetchAppliedJobs = async () => {
-      try {
-        const response = await axios.get(`${apiUrl}/api/user-apply`
-     
-    );
-        setAppliedJobs(response.data.jobApplications);
-      } catch (error) {
-        console.error('Error fetching applied jobs:', error);
-      }
-    };
-
-    fetchAppliedJobs();
-  }, []);
+  const [email, setEmail] = useState("");
+  const fetchAppliedJobs = async () => {
+    const response = await axios.get(`${apiUrl}/api/user-apply?email=${email}`);
+    setAppliedJobs(response.data.jobApplications);
+  };
+  console.log(appliedJobs);
 
   return (
-    <>  
-        
-      <UserHeader/>
-    <div>
-      <h2>Jobs Applied By You</h2>
-      {appliedJobs.length === 0 ? (
-        <p>No jobs applied yet.</p>
-      ) : (
-        <ul>
-          {appliedJobs.map(job => (
-            <li key={job._id}>
-              <h3>{job.jobTitle}</h3>
-              <p>Job ID: {job.jobId}</p>
-              {/* Add more details as needed */}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <UserHeader />
+      <div className="flex">
+        <input type="text" onChange={(e) => setEmail(e.target.value)} />
+        <button onClick={fetchAppliedJobs}>Get Applied Jobs</button>
+      </div>
+      <div>
+        <h2 className="text-center text-lg">Jobs Applied By You</h2>
+        {appliedJobs.length === 0 ? (
+          <p className="text-center mt-20">No jobs applied yet.</p>
+        ) : (
+          <ul>
+            {appliedJobs.map((job) => (
+              <li key={job._id}>
+                <h3>{job.jobTitle}</h3>
+                <p>Job ID: {job.jobId}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </>
-
   );
 };
 
